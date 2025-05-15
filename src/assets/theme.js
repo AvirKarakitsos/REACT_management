e6c93c33084b758d23848bb456fedd382db5cbba
Avir Karakitsos <avir.karakitsos@gmail.com>
2025-05-15 12:15:22 +0200

    feat: custom typography

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -2,26 +2,34 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1e90ff',
-    },
-    secondary: {
-      main: '#EBEDEE',
-    },
-    background: {
-      default: '#fff',
-      paper: '#FAFAFA',
-    },
-    text: {
-      primary: '#000',
-      secondary: '#8D8D8D',
-      third: '#cccccc'
-    },
-    custom: {
-      grey: '#EBEDEE',
-    },
-  },
+	palette: {
+		primary: {
+		main: '#1e90ff',
+		},
+		secondary: {
+		main: '#EBEDEE',
+		},
+		background: {
+		default: '#fff',
+		paper: '#FAFAFA',
+		},
+		text: {
+		primary: '#000',
+		secondary: '#8D8D8D',
+		third: '#cccccc'
+		},
+		custom: {
+		grey: '#EBEDEE',
+		},
+	},
+
+	typography: {
+		h2: {
+		fontSize: 17,
+		fontWeight: 600,
+		marginBottom: 40,
+		},
+	},
 });
 
 export default theme;
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -8,78 +8,85 @@ import EuroIcon from '@mui/icons-material/Euro';
 
 
 export default function MainContent() {
-  return (
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        p: 3,
-        ml: '240px', // largeur du Drawer
-        mt: '64px', // hauteur du Header
-        backgroundColor: 'backgound.default'
-      }}
-    >
-      <Toolbar /> {/* Permet de pousser le contenu sous l’AppBar si besoin */}
+	return (
+		<Box
+		component="main"
+		sx={{
+			flexGrow: 1,
+			p: 6,
+			ml: '240px', // largeur du Drawer
+			mt: '64px', // hauteur du Header
+			backgroundColor: 'backgound.default'
+		}}
+		>
+		<Toolbar /> {/* Permet de pousser le contenu sous l’AppBar si besoin */}
 
-      {/* Partie Haut */}
-      <Box
-        sx={{
-          height: '200px',
-          display:'flex',
-          justifyContent: 'space-evenly',
-		      alignItems:'center',
-          border: '1px solid black',
-          mb: 3
-        }}
-      >
-        <MiniCard title="Items en stock" number='3'>
-			<CercleContainer>
-				<Inventory2Icon fontSize="small" />
-			</CercleContainer>
-		</MiniCard>
-		<MiniCard title="Items en ligne" number='5'>
-			<CercleContainer>
-				<LaptopIcon fontSize="small" />
-			</CercleContainer>
-		</MiniCard>
-		<MiniCard title="Gain Total" number='253 €'>
-			<CercleContainer>
-				<EuroIcon fontSize="small" />
-			</CercleContainer>
-		</MiniCard>
-
-      </Box>
-
-      {/* Partie Milieu */}
-      <Box
-        sx={{
-          height: '400px',
-          backgroundColor: '#e0f7fa',
-          borderRadius: 2,
-          mb: 3,
-          p: 2,
-        }}
-      >
-        <Typography variant="h6" gutterBottom>
-          Les ventes du jour
-        </Typography>
-        {/* Contenu à ajouter ici */}
-      </Box>
-
-      {/* Partie Bas */}
+		{/* Partie Haut */}
 		<Box
 			sx={{
-			minHeight: '200px',
-			backgroundColor: '#fce4ec',
+			height: '200px',
+			display:'flex',
+			justifyContent: 'space-evenly',
+				alignItems:'center',
+			border: '1px solid black',
+			mb: 3
+			}}
+		>
+			<MiniCard title="Items en stock" number='3'>
+				<CercleContainer>
+					<Inventory2Icon fontSize="small" />
+				</CercleContainer>
+			</MiniCard>
+			<MiniCard title="Items en ligne" number='5'>
+				<CercleContainer>
+					<LaptopIcon fontSize="small" />
+				</CercleContainer>
+			</MiniCard>
+			<MiniCard title="Gain Total" number='253 €'>
+				<CercleContainer>
+					<EuroIcon fontSize="small" />
+				</CercleContainer>
+			</MiniCard>
+
+		</Box>
+
+		{/* Partie Milieu */}
+		<Box
+			sx={{
+			height: '400px',
+			backgroundColor: '#e0f7fa',
 			borderRadius: 2,
+			mb: 3,
 			p: 2,
 			}}
 		>
-        <Typography variant="h6" gutterBottom>
-			Les dernières ventes
-        </Typography>
-		<Grid/>
-      </Box>
-    </Box>
-  );
+			<Typography variant="h2">
+				Les ventes du mois
+			</Typography>
+
+			<Box sx={ {display: 'flex'} }>
+				<Typography sx={ {fontSize: '50px', } }>
+					35 €
+				</Typography>
+				{/* Graph */}
+			</Box>
+
+		</Box>
+
+		{/* Partie Bas */}
+			<Box
+				sx={{
+				minHeight: '200px',
+				backgroundColor: '#fce4ec',
+				borderRadius: 2,
+				p: 2,
+				}}
+			>
+			<Typography variant="h2">
+				Les dernières ventes
+			</Typography>
+			<Grid/>
+		</Box>
+		</Box>
+	);
 }
