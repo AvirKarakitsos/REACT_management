d12abd1fd37627da6e4e36d8bc24757892e4dcf1
Avir Karakitsos <avir.karakitsos@gmail.com>
2025-05-21 10:14:19 +0200

    feat: Grid in Analytic page

--- src/components/tools/Grid.jsx
+++ src/components/tools/Grid.jsx
@@ -2,7 +2,7 @@ import { DataGrid } from '@mui/x-data-grid';
 import { Box } from '@mui/material';
 
 
-export default function Grid( {rows,columns } ) {
+export default function Grid( {rows, columns } ) {
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
--- src/pages/Analytic.jsx
+++ src/pages/Analytic.jsx
@@ -1,7 +1,23 @@
+import { Box } from '@mui/material';
+import Grid from '../components/tools/Grid'
+import {data} from '../utilities/common.js'
+
 export default function Analytic() {
     return (
-        <h1>
-            Analytic
-        </h1>
+       <Box
+                component="main"
+                sx={{
+                    flexGrow: 1,
+                    p: 4,
+                    ml: '240px', // largeur du Drawer
+                    mt: '64px', // hauteur du Header
+                    backgroundColor: 'background.paper'
+                }}
+            >
+
+
+            <Grid columns={data.columns} rows={data.rows}/>
+                
+        </Box>
     );
 }
