299e73a1898b8d36b361cdc9ac12c817b4751bfe
Avir Karakitsos <avir.karakitsos@gmail.com>
2025-06-04 21:07:22 +0200

    feat: availableOn for articles in online page

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,11 +1,20 @@
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import {IconButton} from '@mui/material'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from './Modal'
+import { useLocation } from 'react-router'
+
+
 
 export default function Action({row}) {
+    let location = useLocation()
     const [open, setOpen] = useState(false);
+    const [state, setState] = useState({
+        website: null,
+        error: null,
+        loading: true,
+    });
 	
 	const handleEdit = () => {
 		setOpen(true)
@@ -19,6 +28,29 @@ export default function Action({row}) {
 		setOpen(false);
 	};
 
+    useEffect(() => {
+        if(location.pathname === "/online") {
+
+            setState({ website: null, error: null, loading: true });
+    
+            fetch(`http://localhost:4000/api/articles/online/${row.id}`)
+            .then((res) => {
+                if (!res.ok) {
+                throw new Error(`Erreur HTTP: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setState({ website: data, error: null, loading: false });
+            })
+            .catch((err) => {
+                setState({ website: null, error: err.message, loading: false });
+            })
+        }
+
+    },[location.pathname,row.id])
+
+
     return(
         <>
             <IconButton >
@@ -27,6 +59,17 @@ export default function Action({row}) {
             <IconButton>
                 <DeleteIcon onClick={() => handleDelete(row)}/>
             </IconButton>
+
+            {!state.loading 
+                ? <>
+                    {state.website.result.map(item =>(
+                        <IconButton key={item.id}>
+                            <img src={`/logos${item.logoShort}`} style={ {height:"20px",width:"20px"}}/>
+                        </IconButton>)
+                    )}
+                </>
+                : null
+            }
 
             <Modal open={open} handleClose={handleClose} data={row}/>
         </>
